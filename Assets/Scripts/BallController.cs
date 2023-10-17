91860442bab0078d9ee092e15d0caaba535f1eab
Potter72 <elliot.potter72@gmail.com>
2023-10-17 22:21:46 +0200

    ball displacement and movement
    kinda janky movement and camera stuff added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,32 +6,30 @@ using UnityEngine.InputSystem;
 public class BallController : MonoBehaviour
 {
     private int movementTouchId;
-    private int aimingTouchId;
 
-    private Touch movementTouch;
-    private Touch aimingtTouch;
-
-
-    private Vector2 movingStartPos;
     private bool isMoving;
-    private Vector2 aimingStartPos;
-    private bool isAiming;
 
     [SerializeField] float movementForce = 1;
     [SerializeField] float movementForceClamp = 3;
 
     [SerializeField] Rigidbody playerRigidbody;
 
+    [SerializeField] float bounceValueToAdd = 0.6f;
+    [SerializeField] float bounceDecreaseSpeed = 0.1f;
+    [SerializeField] float minBounceValue = 0.1f;
+    [SerializeField] float bounceWobble = 0;
+    [SerializeField] float fallDamageWobble = 1;
+
+    private float startHeight;
+    [SerializeField] float fallDamageCutoff = 1;
+    [SerializeField] Material ballMat;
+
     public void StartMovement()
     {
-        Debug.Log("starting");
-        Debug.Log("touches" + Touchscreen.current.touches);
         for (int i = 0; i < Touchscreen.current.touches.Count; i++)
         {
-
             if (Touchscreen.current.touches[i].phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Began)
             {
-                Debug.Log("succes");
                 //movementTouch = Touchscreen.current.touches[i];
                 movementTouchId = i;
                 isMoving = true;
@@ -45,25 +43,27 @@ public class BallController : MonoBehaviour
         movementTouchId = 1000;
     }
 
-    public void StartAiming()
-    {
-        for (int i = 0; i < Touchscreen.current.touches.Count; i++)
-        {
-            if (Touchscreen.current.touches[i].phase.valueSizeInBytes == 1)
-            {
-                //movementTouch = Touchscreen.current.touches[i];
-                aimingTouchId = i;
-            }
-        }
-    }
-
     public void StopAiming()
     {
 
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            bounceWobble += bounceValueToAdd;
+            Debug.Log("bruh?");
+        }
+    }
+
     private void FixedUpdate()
     {
+        bounceWobble -= bounceDecreaseSpeed * bounceWobble;
+        bounceWobble = Mathf.Clamp(bounceWobble, minBounceValue, 1);
+
+        ballMat.SetFloat("_BounceAmount", bounceWobble);
+
         MovementPhysics();
     }
 
@@ -74,9 +74,30 @@ public class BallController : MonoBehaviour
         //movement forces
         Vector2 movementReadVector = Touchscreen.current.touches[movementTouchId].startPosition.ReadValue() - Touchscreen.current.touches[movementTouchId].position.ReadValue();
 
-        Vector3 movementVector = new Vector3(movementReadVector.x, 0, movementReadVector.y);
+        Vector3 forward = Camera.main.transform.forward;
+        forward = new Vector3(forward.x, 0, forward.z).normalized;
+        Vector3 right = Camera.main.transform.right;
+        right = new Vector3(right.x, 0, right.z).normalized;
+
+        Vector3 movementVector = forward * -movementReadVector.y + right * -movementReadVector.x;
         movementVector = Vector3.ClampMagnitude(movementVector, movementForceClamp);
         //movementVector = Camera.main.transform.forward * movementVector;
         playerRigidbody.AddForce(movementVector * movementForce);
+
+        Debug.DrawRay(movementVector, transform.position,Color.red,2);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        startHeight = transform.position.y;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(startHeight - transform.position.y > fallDamageCutoff) 
+        {
+            bounceWobble += (startHeight - transform.position.y) * fallDamageWobble;
+            startHeight = transform.position.y;
+        }
     }
 }
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    public float smoothSpeed;
+    public GameObject cameraPivot;
+    private float distanceToPlayer;
+
+    public bool canRotateCamera = true;
+
+    //camera shake
+    public float shakeDecreaseSpeed = 1;
+    public float trauma;        //trauma is the current camera shake value and continously lowered
+    public float maxOffset = 1000;
+    public float maxAngle = 9000;
+
+    private float offsetX;
+    private float offsetY;
+    private float offsetZ;
+    private float offsetAngle;
+    private Vector3 shakeOffset;
+
+    public float Sensitivity
+    {
+        get { return sensitivity; }
+        set { sensitivity = value; }
+    }
+    [Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
+    [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
+    [Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
+
+    Vector2 rotation = Vector2.zero;
+    const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
+    const string yAxis = "Mouse Y";
+
+    private void Start()
+    {
+        //offset = transform.localPosition;
+
+    }
+
+    private void Update()
+    {
+        //Lowers trauma over time
+        trauma = Mathf.Clamp((trauma - 1f * Time.deltaTime * shakeDecreaseSpeed), 0, 1);
+
+        //Generates the camera shake offsets if trauma is above 0
+        if (trauma > 0)
+        {
+            offsetAngle = maxAngle * trauma * (Mathf.PerlinNoise(Random.Range(0, 10), Time.time * 15) * 2 - 1);
+            offsetX = maxOffset * trauma * (Mathf.PerlinNoise(Random.Range(0, 10), Time.time * 15) * 2 - 1);
+            offsetY = maxOffset * trauma * (Mathf.PerlinNoise(Random.Range(0, 10), Time.time * 15) * 2 - 1);
+            offsetZ = maxOffset * trauma * (Mathf.PerlinNoise(Random.Range(0, 10), Time.time * 15) * 2 - 1);
+        }
+        else
+        {
+            offsetAngle = 0;
+            offsetX = 0;
+            offsetY = 0;
+            offsetZ = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        distanceToPlayer = (cameraPivot.transform.position - target.transform.position).magnitude;
+        cameraPivot.transform.position = Vector3.Lerp(cameraPivot.transform.position, target.transform.position, Time.deltaTime * smoothSpeed * distanceToPlayer);
+
+        //Adds the camera Shake
+        transform.localPosition = offset;
+        transform.LookAt(target.transform.position);
+        shakeOffset = new Vector3(offsetX, offsetY, offsetZ);
+        transform.position += shakeOffset;
+        if (trauma <= 0)
+        {
+
+        }
+
+        /*
+		if (trauma > 0)
+		{
+			transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z + offsetAngle);
+		}
+		else { transform.rotation = Quaternion.Euler(0, 0, 0); }
+		*/
+    }
+}
