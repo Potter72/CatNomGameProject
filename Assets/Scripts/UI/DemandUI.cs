0e418f3b269b568581c9226e1df0e815a60cebb3
Danny-Bui <114994993+Danny-Bui@users.noreply.github.com>
2023-11-06 21:33:09 +0100

    Demand UI Updated

--- Assets/Scripts/UI/DemandUI.cs
+++ Assets/Scripts/UI/DemandUI.cs
@@ -9,9 +9,11 @@ public class DemandUI : MonoBehaviour
 {
     [SerializeField] private List<Image> _imagePrefabs;
     [SerializeField] private Image _image;
+    [SerializeField] private Image _completeImage;
     [SerializeField] private Transform _catGod;
     [SerializeField] private List<Vector2> _dimensions;
     [SerializeField] private List<Vector2> _slots;
+    [SerializeField] private List<Image> _staticSlots;
 
     private List<Image> _demandImages;
     private List<TextMeshProUGUI> _textBoxes;
@@ -25,6 +27,7 @@ public class DemandUI : MonoBehaviour
     
     private Camera _camera;
 
+    private int _currentCount;
     private bool _outside = false;
 
     private void Awake()
@@ -42,67 +45,55 @@ public class DemandUI : MonoBehaviour
         SetBubblePosition();
     }
 
-    private void SetBubblePosition()
-    {
-        Vector3 screenPoint = _camera.WorldToScreenPoint(_catGod.position);
-
-        if (screenPoint.x < _uiHalfWidth || screenPoint.x > _screenSize.x - _uiHalfWidth || screenPoint.y < _uiHalfHeight || screenPoint.y > _screenSize.y - _uiHalfHeight)
-        {
-            if (!_outside)
-            {
-                _outside = true;
-                _image.color = Color.grey;
-            }
-        }
-        else
-        {
-            if (_outside)
-            {
-                _outside = false;
-                _image.color = Color.white;
-            }
-        }
-        
-        screenPoint.x = Mathf.Clamp(screenPoint.x, _uiHalfWidth, _screenSize.x - _uiHalfWidth);
-        screenPoint.y = Mathf.Clamp(screenPoint.y, _uiHalfHeight, _screenSize.y - _uiHalfHeight);
-
-        _image.rectTransform.position = screenPoint;
-    }
-
     public void SetDemand(List<Item.ItemType> types, List<int> amount)
     {
         SetDimensions(types.Count);
         _amount = amount;
-        Debug.Log(_amount[0]);
         
         for (int i = 0; i < types.Count; i++)
         {
             Image newDemand = GetImage(types[i]);
             TextMeshProUGUI newText = newDemand.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-            
-            newDemand.rectTransform.localPosition = _slots[i];
-            
             newText.text = $"x{amount[i]}";
             _demandImages.Add(newDemand);
             _textBoxes.Add(newText);
             _animators.Add(newText.GetComponent<Animator>());
         }
+        
+        if (!_outside)
+        {
+            SetToDynamicUI();
+        }
+            
+        else if (_outside)
+        {
+            SetToStaticUI();
+        }
     }
-
+    
     public void ReduceByOne(int index)
     {
         _textBoxes[index].text = $"x{_amount[index]}";
         _animators[index].SetTrigger("Bounce");
     }
 
-    public void RemoveType(int index)
+    public void SetTypeComplete(int index)
     {
-        Destroy(_demandImages[index].gameObject);
-        _demandImages.RemoveAt(index);
-        _textBoxes.RemoveAt(index);
-        _animators.RemoveAt(index);
+        Instantiate(_completeImage, _demandImages[index].transform, false);
     }
-
+    
+    public void RemoveAllTypes()
+    {
+        for (int i = 0; i < _demandImages.Count; i++)
+        {
+            Destroy(_demandImages[i].gameObject);
+        }
+        
+        _demandImages.Clear();
+        _textBoxes.Clear();
+        _animators.Clear();
+    }
+    
     private Image GetImage(Item.ItemType type)
     {
         Image i;
@@ -160,23 +151,16 @@ public class DemandUI : MonoBehaviour
 
     private void SetDimensions(int count)
     {
+        _currentCount = count;
+        
         switch (count)
         {
-            // case 1:
-            //     _image.rectTransform.sizeDelta = _dimensions[0];
-            //     break;
-            // case 2:
-            //     _image.rectTransform.sizeDelta = _dimensions[1];
-            //     break;
-            // case 3:
-            //     _image.rectTransform.sizeDelta = _dimensions[2];
-            //     break;
-            // case 4:
-            //     _image.rectTransform.sizeDelta = _dimensions[2];
-            //     break;
-            // case 5:
-            //     _image.rectTransform.sizeDelta = _dimensions[3];
-            //     break;
+            case 1:
+                _image.rectTransform.sizeDelta = _dimensions[0];
+                break;
+            case 3:
+                _image.rectTransform.sizeDelta = _dimensions[1];
+                break;
             default:
                 _image.rectTransform.sizeDelta = _dimensions[0];
                 break;
@@ -184,5 +168,70 @@ public class DemandUI : MonoBehaviour
 
         _uiHalfWidth = _image.rectTransform.sizeDelta.x / 2;
         _uiHalfHeight = _image.rectTransform.sizeDelta.y / 2;
+    }
+    
+    private void SetBubblePosition()
+    {
+        Vector3 screenPoint = _camera.WorldToScreenPoint(_catGod.position);
+
+        if (screenPoint.x < _uiHalfWidth || screenPoint.x > _screenSize.x - _uiHalfWidth || screenPoint.y < _uiHalfHeight || screenPoint.y > _screenSize.y - _uiHalfHeight)
+        {
+            if (!_outside)
+            {
+                _outside = true;
+                // _image.color = Color.grey;
+                SetToStaticUI();
+            }
+        }
+        else
+        {
+            if (_outside)
+            {
+                _outside = false;
+                // _image.color = Color.white;
+                SetToDynamicUI();
+            }
+        }
+        
+        screenPoint.x = Mathf.Clamp(screenPoint.x, _uiHalfWidth, _screenSize.x - _uiHalfWidth);
+        screenPoint.y = Mathf.Clamp(screenPoint.y, _uiHalfHeight, _screenSize.y - _uiHalfHeight);
+
+        _image.rectTransform.position = screenPoint;
+    }
+
+    private void SetToDynamicUI()
+    {
+        _image.color = new Color(1, 1, 1, 1);
+        
+        for (int i = 0; i < _staticSlots.Count; i++)
+        {
+            _staticSlots[i].color = new Color(1, 1, 1, 0);
+        }
+        
+        for (int i = 0; i < _demandImages.Count; i++)
+        {
+            _demandImages[i].rectTransform.SetParent(transform);
+            _demandImages[i].rectTransform.localPosition = _slots[i];
+        }
+    }
+
+    private void SetToStaticUI()
+    {
+        _image.color = new Color(1, 1, 1, 0);
+        
+        if (_currentCount == 1)
+        {
+            _staticSlots[0].color = new Color(1, 1, 1, 1);
+            _demandImages[0].rectTransform.position = _staticSlots[0].rectTransform.position;
+            _demandImages[0].rectTransform.SetParent(_staticSlots[0].rectTransform);
+            return;
+        }
+
+        for (int i = 0; i < _staticSlots.Count; i++)
+        {
+            _staticSlots[i].color = new Color(1, 1, 1, 1);
+            _demandImages[i].rectTransform.position = _staticSlots[i].rectTransform.position;
+            _demandImages[i].rectTransform.SetParent(_staticSlots[i].rectTransform);
+        }
     }
 }
