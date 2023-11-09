164b7d6ad0e33925925984236873cf63413f18b8
Potter72 <elliot.potter72@gmail.com>
2023-11-09 15:46:10 +0100

    updated end text

--- Assets/ShadersEffects/CatGod/EndParticles/CatGodEndGameExplosion.cs
+++ Assets/ShadersEffects/CatGod/EndParticles/CatGodEndGameExplosion.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.VFX;
 
 public class CatGodEndGameExplosion : MonoBehaviour
@@ -13,6 +17,11 @@ public class CatGodEndGameExplosion : MonoBehaviour
     [SerializeField] VisualEffect catExplodeVFX;
     [SerializeField] GameObject catGodRendererGO;
     [SerializeField] Material catGodMaterial;
+
+    //credits
+    [SerializeField] GameObject endCreditImage;
+    [SerializeField] GameObject endCreditText;
+    
 
     // Update is called once per frame
     void Update()
@@ -49,7 +58,31 @@ public class CatGodEndGameExplosion : MonoBehaviour
         catExplodeVFX.SetFloat("TrailsSpawnRate", 0);
         catGodRendererGO.SetActive(false);
         catGodMaterial.SetFloat("_Warble_Amplitude", 0);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(4);
         catExplodeVFXGO.SetActive(false);
+
+        
+        //end game credits   
+        endCreditImage.SetActive(true);
+        endCreditText.SetActive(true);
+
+        UnityEngine.UI.Image endImage = endCreditImage.GetComponent<UnityEngine.UI.Image>();
+        TextMeshProUGUI endText = endCreditText.GetComponent<TextMeshProUGUI>();
+        float creditTime = 2;
+        float counter2 = 0;
+        float startTime2 = Time.time;
+
+        Color startColorImage = endImage.color;
+        Color endColorImage = new Color(0.02f, 0.02f, 0.02f, 0.93f);
+        Color startColorText = endText.color;
+        Color endColorText = new Color(0.97f, 0.97f, 0.97f, 1);
+
+        while (Time.time - startTime2 < creditTime)
+        {
+            endImage.color = Color.Lerp(startColorImage, endColorImage, counter2 / creditTime);
+            endText.color = Color.Lerp(startColorText, endColorText, counter2 / creditTime);
+            counter2 += Time.deltaTime;
+            yield return 0;
+        }
     }
 }
