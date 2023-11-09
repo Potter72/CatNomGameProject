8db446ee82367b97becc84929e436f24fdcc95b8
Danny-Bui <114994993+Danny-Bui@users.noreply.github.com>
2023-11-09 15:06:51 +0100

    Fox is no longer Jesus

--- Assets/Scripts/Environmental/RandomItemSpawner.cs
+++ Assets/Scripts/Environmental/RandomItemSpawner.cs
@@ -20,6 +20,7 @@ public class RandomItemSpawner : MonoBehaviour
     }
 
     [SerializeField] private List<SpawnAreaList> _spawnAreas;
+    [SerializeField] private int _group;
     [SerializeField] private Color _gizmoColor;
 
     [SerializeField] private Transform _parent;
@@ -41,7 +42,7 @@ public class RandomItemSpawner : MonoBehaviour
     {
         Gizmos.color = _gizmoColor;
         
-        foreach (SpawnArea s in _spawnAreas[0].SpawnAreaPoints)
+        foreach (SpawnArea s in _spawnAreas[_group].SpawnAreaPoints)
         {
             Gizmos.DrawSphere(s.Point.position, s.Radius);
         }
